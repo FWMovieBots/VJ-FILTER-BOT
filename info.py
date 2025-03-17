4eada869c59229f6b4a273850be6988261843c22
FWMovieBots <jatr14151@gmail.com>
2025-03-17 18:51:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -60,8 +60,8 @@ DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in enviro
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jatr14151:M4jhDPOMUkxNzJML@cluster0.9domc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")   # IF Multiple Database Is False Then Fill Only This Database Url.
-DATABASE_NAME = environ.get('DATABASE_NAME', "techvjclonefilterbot")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjcollection')
+DATABASE_NAME = environ.get('DATABASE_NAME', "fwmovieclonefilterbot")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'fwcollection')
 
 MULTIPLE_DATABASE = bool(environ.get('MULTIPLE_DATABASE', False)) # Set True or False
 
