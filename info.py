4544fda3fdfb09d538d1b8e19cd8ff1baece73ab
FWMovieBots <jatr14151@gmail.com>
2025-06-06 11:03:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -125,10 +125,10 @@ VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', '')
 
 
 # Shortlink Info
-SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', False)) # Set True Or False
-SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
-SHORTLINK_API = environ.get('SHORTLINK_API', '')
-TUTORIAL = environ.get('TUTORIAL', '') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
+SHORTLINK_MODE = bool(environ.get('SHORTLINK_MODE', True)) # Set True Or False
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'shortxlinks.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'e72a0610e358441ca9747355e6f95288c52c7b10')
+TUTORIAL = environ.get('TUTORIAL', 'https://t.me/dglinkopen/53') # How Open Shortner Link Video Link , Channel Link Where You Upload Your Video.
 
 
 # Others
